3e6be3d448d81c42470ea6dcfdc76da64539b21d
JoergZ2 <joerg.zywek@gmx.de>
2025-04-24 12:17:36 +0200

    Initial upload 20250424

---
+++ README.md
@@ -0,0 +1,38 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/list.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/>
+# ovos-skill-my-foxess-cloud
+OVOS skill to fetch data from FoxESS cloud.
+## Summary
+This skill currently only uses a small part of the information collected by the FoxESS cloud. At present, only current data is retrieved. Retrieving cumulative data from the past is planned but not yet implemented. English and German intents and dialogs have been created for the skill. Other languages must be added yourself.
+## Which intents are used (examples)
+"What does the photovoltaic system deliver (just|current|)" - current power production from solar panels in kW
+"How much electricity is (currently|) (used|consumed|in the house|by us|)" - current consumption of all devices in kW
+"Current feed-in power" - current export to grid in kW
+"(What is the|) efficiency of the inverter" - calculates the lost of the system 
+
+There are some more intents. You can find then in ```locale/en-us/intents``` folder.
+## Known issues
+During installing the neccessary libraries from a fresh version of numpy (2.2.5) ist installed. This produced a conflict message with ovos-classifiers. The maintainer of OVOS told me that doesn't matter.
+## Setup
+You have to edit settings.json of the skill. You will find it in your config folder in ```...skills/ovos-skill-my-foxess-cloud.joergz2/settings.json``` It looks like:
+```
+{
+    "__mycroft_skill_firstrun": false,
+    "api_key": "12345678-9876-3541...",
+    "device_sn": "60SH......",
+    "time_zone": "Europe/Berlin",
+    "lang_specifics": {
+        "decimal_char": ","
+    }
+}
+```
+## Notes
+"api_key": Your supplier or installer should give access data for foxesscloud.com. After you configured an account go to User profile and API Management. You can generate your own api key(s).
+"device_sn": ask your supplier or installer.
+"time_zone": self explaining, isn't it?
+"lang_specifics": In this skill only "decimal_char" is used. All digit values from FoxESS cloud are of type 'float'. For some countries - e. g. Germany - the decimal point is a comma (,). For correct speaking the point from float has to change to comma. If you don't need replacing, let "." as decimal point (default).  
+##Sources and more information
+https://github.com/TonyM1958/FoxESS-Cloud
+https://github.com/macxq/foxess-ha/wiki/Understand-PV-string-power-generation-using-foxess-ha
+https://foxesscommunity.com/
+## Questions and issues
+Use "Issues" from this github pages.
---
+++ __init__.py
@@ -0,0 +1,166 @@
+from ovos_utils import classproperty
+from ovos_utils.log import LOG
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
+from ovos_bus_client.session import SessionManager
+from threading import Event
+import foxesscloud.openapi as f
+import json
+#Just a comment
+power_vars = f.power_vars
+battery_vars = f.battery_vars
+energy_vars = f.energy_vars
+DEFAULT_SETTINGS = {
+    "__mycroft_skill_firstrun": "False",
+    "api_key": "Your_API_Key",
+    "device_sn": "Your_inverter_serial_number",
+    "time_zone": "Europe/Berlin",
+    "lang_specifics": {
+        "decimal_char": "."
+    }
+}
+
+class FoxESSCloudSkill(OVOSSkill):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # be aware that below is executed after `initialize`
+        self.override = True
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(
+            internet_before_load=False,
+            network_before_load=False,
+            gui_before_load=False,
+            requires_internet=False,
+            requires_network=False,
+            requires_gui=False,
+            no_internet_fallback=True,
+            no_network_fallback=True,
+            no_gui_fallback=True,
+        )
+    
+    def initialize(self):
+        #from template
+        self.settings.merge(DEFAULT_SETTINGS, new_only=True)
+        self.settings_change_callback = self.on_settings_changed
+        f.api_key = self.settings.get("api_key", None)
+        f.device_sn = self.settings.get("device_sn", None)
+        f.time_zone = self.settings.get("time_zone", None)
+        self.lang_specifics = self.settings.get("lang_specifics", None)
+
+    def on_settings_changed(self):
+        """This method is called when the skill settings are changed."""
+    
+    #Main FoxESS functions
+    def realdata(self,vars=None):
+        """Returns all data since last datalogger sendings to cloud. This
+        function is specified by intents. vars could be None, a single FoxESS
+        var or a list of FoxESS vars."""
+        if vars == None:
+            result = f.get_real()
+        else:
+            result = f.get_real(vars)
+        #result = json.loads(result)
+        return result
+    
+    #Helpers
+    def round3_realdata(self,result):
+        """Function rounds long float to 3 digits after decimal point"""
+        if len(result) == 1:
+            result = round(result[0]['value'], 3)
+            return result
+        i = 0
+        while i < len(result):
+            result[i]['value'] = round(result[i]['value'], 3)
+            i += 1
+        return result
+    
+    def prepare_values(self, selection,values):
+        """Function for localization and to prepare a dict of values for TTS"""
+        result = {}
+        i = 0
+        while i < len(selection):
+            new_value = values[i]['value']
+            new_value = str(new_value).replace(".",self.lang_specifics['decimal_char'])
+            values[i]['value'] = new_value
+            result.update({selection[i]: str(values[i]['value'])})
+            i += 1
+        return result
+
+    
+    #Intents
+    @intent_handler('current_pvpower.intent')
+    def handle_current_pvpower(self, message):
+        """Returns photovoltaic output data"""
+        selection = "pvPower"
+        result = self.realdata(selection)
+        value = str(result[0]['value']).replace(".",self.lang_specifics['decimal_char'])
+        LOG.info("Result ist: " + str(result[0]) + " value ist: " + str(value))
+        self.speak_dialog('current_pvpower',{'current_energy': value})
+    
+    @intent_handler('current_delivery.intent')
+    def handle_current_delivery(self, message):
+        """Returns power export data"""
+        selection = "feedinPower"
+        result = self.realdata(selection)
+        value = self.round3_realdata(result)
+        value = str(value).replace(".",self.lang_specifics['decimal_char'])
+        self.speak_dialog('current_delivery', {'energy_delivery': value})
+
+    @intent_handler('current_consumption.intent')
+    def handle_current_consumption(self, message):
+        """Returns current energy consumption data"""
+        selection = "loadsPower"
+        result = self.realdata(selection)
+        value = self.round3_realdata(result)
+        value = str(value).replace(".",self.lang_specifics['decimal_char'])
+        self.speak_dialog('current_loads', {'current_loads': value})
+
+    @intent_handler('current_bat_level.intent')
+    def handle_current_bat_level(self, message):
+        """Returns filling level of battery"""
+        selection = "SoC"
+        result = self.realdata(selection)
+        value = str(result[0]['value']).replace(".",self.lang_specifics['decimal_char'])
+        self.speak_dialog('current_bat_level', {'bat_level': value})
+
+    @intent_handler('current_grid_consumption.intent')
+    def handle_current_grid_consumption(self, message):
+        """Returns current power import from grid"""
+        selection = "gridConsumptionPower"
+        result = self.realdata(selection)
+        value = self.round3_realdata(result)
+        value = str(value).replace(".",self.lang_specifics['decimal_char'])
+        self.speak_dialog('current_grid_consumption', {'grid_consumption': value})
+    
+    @intent_handler('current_efficiency_of_inverter.intent')
+    def handle_current_inverter_efficiency(self, message):
+        """Returns production and generation date and calculates efficiency"""
+        selection = ["pvPower", "generationPower", "batChargePower", "feedinPower"]
+        result = self.realdata(selection)
+        efficiency = round((result[1]['value']+result[2]['value']+result[3]['value'])/result[0]['value']*100, 0)
+        values = self.round3_realdata(result)
+        values = self.prepare_values(selection, values)
+        self.speak_dialog('current_efficiency_of_inverter', {'efficiency': efficiency})
+
+    @intent_handler('current_inv_bat_charge.intent')
+    def handle_current_inv_bat_charge(self, message):
+        selection = "batChargePower"
+        result = self.realdata(selection)
+        value = str(result[0]['value']).replace(".",self.lang_specifics['decimal_char'])
+        self.speak_dialog('current_inv_bat_charge', {'batChargePower': value})
+
+
+    @intent_handler('current_energy_balance.intent')
+    def handle_current_energy_balance(self, message):
+        """Returns current data of prduction, consumption, state of battery..."""
+        selection = ['generationPower', 'feedinPower', 'loadsPower', 'gridConsumptionPower', 'batChargePower', 'batDischargePower', 'pvPower']
+        result = self.realdata(selection)
+        values = self.round3_realdata(result)
+        values = self.prepare_values(selection, values)
+        self.speak_dialog('current_energy_balance', {'pvPower': values['pvPower'], 'batChargePower': values['batChargePower'], \
+                                                     'loadsPower': values['loadsPower'], 'gridConsumptionPower': values['gridConsumptionPower'], \
+                                                        'batDischargePower': values['batDischargePower'], 'generationPower': values['generationPower'], \
+                                                            'feedinPower': values['feedinPower']})
